ce701f273822f7e959d33f97c204b5527b4e4f55
trashpanda31 <e.smailik@yandex.com>
2024-11-16 12:40:49 -0500

    message

---
+++ followersCount/followers_count.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+function increaseCount(){
+    count++;
+
+displayCount();
+checkCountValue();
+}
+
+function checkCountValue() {
+    if (count === 10){
+    alert ("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20){
+      alert ("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
+
+function displayCount() {
+    document.getElementById(`countDisplay`).innerHTML=count;
+
+}
+
